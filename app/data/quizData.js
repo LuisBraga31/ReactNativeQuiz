570d3563e64438870d9e2b99abe4e7a27980f843
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-09-02 14:46:44 -0300

    Setup do projeto

---
+++ app/data/quizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "Qual o maior planeta do nosso sistema solar?",
+        options: ["Jupiter","Saturno","Netuno","Mercúrio"],
+        correct_option: "Jupiter"
+    },
+    {
+        question: "Qual o monumento da Índia é um dos mais famosos do mundop ?",
+        options: ["Chand Minar","Taj Mahal","Stadium"],
+        correct_option: "Taj Mahal"
+    },
+    {
+        question: "Qual animal terrestre consegue abrir mais a boca?",
+        options: ["Jacaré","Crocodilo","Babuíno","Hipopótamo"],
+        correct_option: "Hipopótamo"
+    },
+    {
+        question: "Qual o maior animal da terra?",
+        options: ["Elefecante Africano","Baleia Azul","Cachalote","Lula gigante"],
+        correct_option: "Baleia Azul"
+    },
+    {
+        question: "Qual é o único mamífero voador?",
+        options: ["Morcego","Esquilo voador","Águia Careca","Borboleta"],
+        correct_option: "Morcego"
+    }
+]
